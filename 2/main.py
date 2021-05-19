8b4ca41506b0dade93062beaa97bc9ec6650701c
Artemka1806 <74206792+Artemka1806@users.noreply.github.com>
2021-05-19 17:25:33 +0300

    1

--- 2/main.py
+++ 2/main.py
@@ -4,28 +4,21 @@ from tkinter import messagebox
 import tkinter as tk
 from tkinter import ttk 
 from tkinter import filedialog
-from tkinter.ttk import Combobox
+from langdetect import detect
 
 root = Tk()
 root.title("Синтезатор речи")
-root.geometry("185x67")
+root.geometry("185x50")
 root.resizable(width=0, height=0)
 
 entry = Entry( width=30,relief = 'flat')
 entry.grid(row=5,column=0, padx=0, pady=1)
 
-comboExample = ttk.Combobox(root, 
-                            values=[
-                                    "ru", 
-                                    "en"])
-comboExample.grid(column=0, row=1)
-comboExample.current(1)
-
 button1 = Button(text="Сохранить",relief = 'flat',command=lambda: se())
 button1.grid(row=6, column=0,sticky="e")
 
 def se():
-    tts = gTTS(entry.get(), lang=comboExample.get())
+    tts = gTTS(entry.get(), lang=detect(entry.get()))
     tts.save(filedialog.asksaveasfilename(defaultextension=".mp3",filetypes=(("Audio files", "*.mp3"),
                    ("All files", "*.*"))))
              
